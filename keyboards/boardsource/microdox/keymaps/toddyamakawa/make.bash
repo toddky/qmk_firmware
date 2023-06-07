4661a56db35be7858d57a4d50f9a52d9046fa416
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-06-07 09:38:36 -0500

    microdox: Adding keymap.

---
+++ keyboards/boardsource/microdox/keymaps/toddyamakawa/make.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#qmk_bin=$HOME/.local/bin
+#export PATH="$PATH:$qmk_bin"
+which qmk || exit $?
+
+# brew install avr-gcc
+which avr-gcc || exit $?
+
+#keymap='toddyamakawa'
+keymap='default'
+
+print-run qmk compile -kb boardsource/microdox/v2 -km "$keymap" || exit $?
+
+top="$(git top 2>/dev/null)"
+build="$top/.build"
+hexfile="$build/boardsource_microdox_$keymap.hex"
+dest="$HOME/.config/links/downloads"
+[[ -d "$HOME/Downloads" ]] && dest="$HOME/Downloads"
+
+echo
+echo "\$ cp "$hexfile" "$dest""
+cp "$hexfile" "$dest"
+
