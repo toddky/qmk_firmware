ae99473b3818371773017e24cbf1b0b280417536
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-07-02 11:11:07 -0500

    microdox: Use TAPPING_TERM 0.

--- keyboards/boardsource/microdox/keymaps/toddyamakawa/config.h
+++ keyboards/boardsource/microdox/keymaps/toddyamakawa/config.h
@@ -2,6 +2,10 @@
 #pragma once
 
 #define RETRO_TAPPING
-#define TAPPING_TERM 200
-#define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_TERM 0
+
+// Experiment with HOLD_ON_OTHER_KEY_PRESS failed
+// Typing Backspace then Shift-Letter would cause Backspace to be triggered twice
+//#define TAPPING_TERM 200
+//#define HOLD_ON_OTHER_KEY_PRESS
 
